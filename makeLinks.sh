e402cb94748f1f2ce14b8ac89f5fc72eb165e9ed
Keith Pedersen <kpeders1@hawk.iit.edu>
2018-02-17 12:43:10 -0600

    Add README and helper script to make links

---
+++ makeLinks.sh
@@ -0,0 +1,7 @@
+#~ Wherever you place the git repo directory <iithesis/>,
+#~ you will need links to the class and font files in your main compilation directory (MCD)
+#~ (i.e. where "YourThesis.tex" lives). I place my <iithesis/> folder directly in my MCD. 
+#~ If you do the same, you can run the this script in the MCD to make the required links.
+ln -s iitthesis/iitthesis.cls 
+ln -s iitthesis/font11.clo 
+ln -s iitthesis/font12.clo 
